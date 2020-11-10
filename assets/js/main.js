a4eb435a4cc3ee373d67d863bc7186257e1e945a
xNickolas <nickolsctt@gmail.com>
2020-11-10 17:15:13 -0300

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 2.0, y: 30, stagger: 0.2,})
+gsap.from('.banner__title', {opacity: 0, duration: 1, delay:1.4, y: 30})
+gsap.from('.banner__description', {opacity: 0, duration: 1, delay:1.4, y: 30})
+gsap.from('.banner', {opacity: 0, duration: 0.9, delay: 1, y: 30})
+gsap.from('.floating__img', {opacity: 0, duration: 0.9, delay: 1, y: 30})
+gsap.from('.articles__midle', {opacity: 0, duration: 0.9, delay: 1, y: 30})
+
+
